d5d9a6d867f31a75540f6fcc3bbfe4fa7161c541
tothalex <tothalex097@gmail.com>
2020-06-11 18:38:14 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/Diplomamunka'));
+app.use(express.static(__dirname + '/dist/naturzona-webshop'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/Diplomamunka/index.html'));
+app.get('/.*/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/naturzona-webshop/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
